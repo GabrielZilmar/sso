db1ce9160cd92aa165b8a2db5ba23bd8ef9258a6
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-10-24 22:54:55 -0300

    feat: add entity, identifier and uniqueEntityId domain classes

---
+++ src/shared/domain/Entity.ts
@@ -0,0 +1,31 @@
+import { UniqueEntityID } from "shared/domain/UniqueEntityID";
+
+export abstract class Entity<T> {
+  protected readonly _id: UniqueEntityID;
+  public readonly props: T;
+
+  constructor(props: T, id?: UniqueEntityID) {
+    this._id = id;
+    this.props = props;
+  }
+
+  private isEntity = (v: any): v is Entity<any> => {
+    return v instanceof Entity;
+  };
+
+  public equals(object?: Entity<T>): boolean {
+    if (!object) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!this.isEntity(object)) {
+      return false;
+    }
+
+    return this._id.equals(object._id);
+  }
+}
