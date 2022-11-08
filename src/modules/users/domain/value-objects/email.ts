82b3dfcbc7daf4991bc4793cf03094b6c629430a
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-07 23:32:35 -0300

    feat: add userEmail value-object

---
+++ src/modules/users/domain/value-objects/email.ts
@@ -0,0 +1,36 @@
+import { UserDomainErrors } from "~modules/users/domain/errors";
+import { ValueObject } from "~shared/domain/value-object";
+
+export interface UserEmailProps {
+  value: string;
+}
+
+export default class UserEmail extends ValueObject<UserEmailProps> {
+  get value(): string {
+    return this.props.value;
+  }
+
+  private constructor(props: UserEmailProps) {
+    super(props);
+  }
+
+  private static isValid(email: string) {
+    const isEmailRegex =
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    return isEmailRegex.test(email);
+  }
+
+  private static format(email: string): string {
+    return email.trim().toLowerCase();
+  }
+
+  // TODO: implements an Either
+  public static create(email: string): UserEmail {
+    if (!this.isValid(email)) {
+      throw new Error(UserDomainErrors.invalidEmail);
+    }
+
+    return new UserEmail({ value: this.format(email) });
+  }
+}
