fed0b92d9fee15bc5280626744b4ab7d9cbc4545
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-09 21:20:29 -0300

    feat: add errors for UserDomainErrors

--- src/modules/users/domain/errors.ts
+++ src/modules/users/domain/errors.ts
@@ -1,3 +1,5 @@
 export enum UserDomainErrors {
   invalidEmail = "Email address is not valid.",
+  invalidPassword = "Invalid password. At least 6 characters are required",
+  invalidName = "Invalid name. The name can contains only letters and digits, and at max 15 characters.",
 }
