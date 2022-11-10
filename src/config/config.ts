d9f2d978afc07fe1175bf70e74fbcef6df7a8a37
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-09 21:20:07 -0300

    feat: add password salt to envs

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,7 +4,16 @@ export type Environment = "prod" | "dev";
 
 dotenv.config();
 
+const getPasswordSalt = () => {
+  const passwordSalt = process.env.PASSWORD_SALT as string;
+
+  return passwordSalt ? Number(passwordSalt) : 0;
+};
+
 export default {
   port: process.env.PORT as string,
   env: process.env.NODE_ENV as Environment,
+  get passwordSalt() {
+    return getPasswordSalt();
+  },
 };
