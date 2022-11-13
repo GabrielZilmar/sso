b15615e5635f0c3ab66118c5c8486741974029df
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-13 15:13:23 -0300

    feat: add UserDomainError class

--- src/modules/users/domain/errors.ts
+++ src/modules/users/domain/errors.ts
@@ -4,3 +4,10 @@ export enum UserDomainErrors {
   invalidName = "Invalid name. The name can contains only letters and digits, and at max 15 characters.",
   invalidUserProps = "Couldn't create the user. Invalid props",
 }
+
+export default class UserDomainError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "user-domain";
+  }
+}
