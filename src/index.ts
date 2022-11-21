f2351c55307a105ee087db543d4d5c4132070a6e
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-20 22:03:28 -0300

    feat: initialize data source

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import "reflect-metadata";
 import { config } from "dotenv";
 import ExpressWebServer from "~services/webserver/express/http-server";
 import DependencyInjection from "~shared/dependency-injection";
+import { AppDataSource } from "~services/database/typeorm/data-source";
 
 const main = async () => {
   config();
   DependencyInjection.initialize();
+
+  await AppDataSource.initialize();
 
   const httpServer = DependencyInjection.resolve(ExpressWebServer);
   await httpServer.start();
