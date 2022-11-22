a27716568034c99a036792e702036980d6074b95
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-11-21 22:57:31 -0300

    feat: add User Entity

---
+++ src/modules/users/entity/User.ts
@@ -0,0 +1,36 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  Check,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity()
+@Check(`"deleted_at" IS NULL`)
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: false })
+  isEmailVerified: boolean;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+
+  @Column({ nullable: true })
+  deletedAt: Date;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
