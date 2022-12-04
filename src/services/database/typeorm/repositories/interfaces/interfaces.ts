11ed0765e6612cd5e03062e661861a6e30b2a371
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-12-04 13:45:39 -0300

    feat: add repositories interfaces

---
+++ src/services/database/typeorm/repositories/interfaces/interfaces.ts
@@ -0,0 +1,19 @@
+import { FindOptionsWhere, UpdateResult } from "typeorm";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+import RepositoryError from "~services/database/typeorm/repositories/error";
+import { Either } from "~shared/either";
+
+export interface IWrite<T> {
+  create(item: T): Promise<Either<RepositoryError, T>>;
+  update(
+    id: string,
+    item: QueryDeepPartialEntity<T>
+  ): Promise<Either<RepositoryError, UpdateResult>>;
+  delete(id: string): Promise<Either<RepositoryError, boolean>>;
+}
+
+export interface IRead<T> {
+  find(criteria: FindOptionsWhere<T>): Promise<T[]>;
+  findOneByCriteria(criteria: FindOptionsWhere<T>): Promise<T | null>;
+  findOneById(id: string): Promise<T | null>;
+}
