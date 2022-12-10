070142158e0e66f88a3137132baa6f89b141e828
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-12-10 18:16:59 -0300

    feat: use valueObject insted of only value

--- src/modules/users/domain/user.ts
+++ src/modules/users/domain/user.ts
@@ -4,17 +4,20 @@ import UserDomainError, {
 import { UserCreatedEventPayload } from "~modules/users/domain/events-listeners/user-created";
 import { UserDeletedEventPayload } from "~modules/users/domain/events-listeners/user-deleted";
 import UserId from "~modules/users/domain/user-id";
+import UserEmail from "~modules/users/domain/value-objects/email";
+import UserName from "~modules/users/domain/value-objects/name";
+import UserPassword from "~modules/users/domain/value-objects/password";
 import { AggregateRoot } from "~shared/domain/aggregate-root";
 import { UniqueEntityID } from "~shared/domain/unique-entity-id";
 import { Either, Left, Right } from "~shared/either";
 
 export interface UserProps {
-  email: string;
-  name: string;
-  password: string;
+  email: UserEmail;
+  name: UserName;
+  password: UserPassword;
   isEmailVerified?: boolean;
   isAdmin?: boolean;
-  isDeleted?: boolean;
+  deletedAt?: Date | null;
   // TODO: Jwt tokens
 }
 
@@ -23,15 +26,15 @@ export class UserDomain extends AggregateRoot<UserProps> {
     return UserId.create(this._id);
   }
 
-  get email(): string {
+  get email(): UserEmail {
     return this.props.email;
   }
 
-  get name(): string {
+  get name(): UserName {
     return this.props.name;
   }
 
-  get password(): string {
+  get password(): UserPassword {
     return this.props.password;
   }
 
@@ -43,8 +46,8 @@ export class UserDomain extends AggregateRoot<UserProps> {
     return this.props.isAdmin;
   }
 
-  get isDeleted(): boolean {
-    return this.props.isDeleted;
+  get isDeleted(): Date | null {
+    return this.props.deletedAt;
   }
 
   private constructor(props: UserProps, id?: UniqueEntityID) {
@@ -58,12 +61,12 @@ export class UserDomain extends AggregateRoot<UserProps> {
   }
 
   public async delete(): Promise<void> {
-    if (!this.props.isDeleted) {
-      this.props.isDeleted = true;
+    if (!this.props.deletedAt) {
+      this.props.deletedAt = new Date();
 
       const eventPayload: UserDeletedEventPayload = {
         userId: this.userId,
-        userEmail: this.email,
+        userEmail: this.email.value,
         deletedTime: new Date(),
       };
       await this.emitEvent("user.created", eventPayload);
@@ -83,7 +86,7 @@ export class UserDomain extends AggregateRoot<UserProps> {
     const user = new UserDomain(
       {
         ...props,
-        isDeleted: props.isDeleted || false,
+        deletedAt: props.deletedAt || null,
         isEmailVerified: props.isEmailVerified || false,
         isAdmin: props.isAdmin || false,
       },
