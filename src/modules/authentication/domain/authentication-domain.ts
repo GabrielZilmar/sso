18c5caa3384e6a1fb2a9dbbff0af8f743bace8ac
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-12-18 15:47:25 -0300

    feat: create authentication domain

---
+++ src/modules/authentication/domain/authentication-domain.ts
@@ -0,0 +1,59 @@
+import AuthenticationDomainError, {
+  AuthenticationDomainErrors,
+} from "~modules/authentication/domain/errors";
+import { AuthenticationCreatedEventPayload } from "~modules/authentication/domain/events-listeners/auth-created";
+import AccessToken from "~modules/authentication/domain/value-objects/access-token";
+import AuthenticationUserId from "~modules/authentication/domain/value-objects/authentication-user-id";
+import { AggregateRoot } from "~shared/domain/aggregate-root";
+import { UniqueEntityID } from "~shared/domain/unique-entity-id";
+import { Either, Left, Right } from "~shared/either";
+
+export interface AuthenticationDomainProps {
+  accessToken: AccessToken;
+  userId: AuthenticationUserId;
+}
+
+export default class AuthenticationDomain extends AggregateRoot<AuthenticationDomainProps> {
+  private constructor(props: AuthenticationDomainProps, id?: UniqueEntityID) {
+    super(props, id);
+  }
+
+  get accessToken(): AccessToken {
+    return this.props.accessToken;
+  }
+
+  get userId(): AuthenticationUserId {
+    return this.props.userId;
+  }
+
+  private static isValid(props: AuthenticationDomainProps): boolean {
+    const { accessToken, userId } = props;
+
+    return !accessToken || !userId;
+  }
+
+  public static async create(
+    props: AuthenticationDomainProps,
+    id?: UniqueEntityID
+  ): Promise<Either<AuthenticationDomainError, AuthenticationDomain>> {
+    if (!this.isValid(props)) {
+      return new Left(
+        new AuthenticationDomainError(
+          AuthenticationDomainErrors.invalidAuthenticationProps
+        )
+      );
+    }
+
+    const auth = new AuthenticationDomain({ ...props }, id);
+
+    const isNewAuth = !id;
+    if (isNewAuth) {
+      const eventPayload: AuthenticationCreatedEventPayload = {
+        authentication: auth,
+      };
+      await auth.emitEvent("authentication.created", eventPayload);
+    }
+
+    return new Right(auth);
+  }
+}
--- src/modules/authentication/domain/errors.ts
+++ src/modules/authentication/domain/errors.ts
@@ -1,6 +1,7 @@
 export enum AuthenticationDomainErrors {
   invalidAuthenticationUserId = "Authentication user id is not valid.",
   invalidAuthenticationAccessToken = "Access token is not valid. Invalid signature ou invalid format.",
+  invalidAuthenticationProps = "Couldn't create the authentication. Invalid props",
 }
 
 export default class AuthenticationDomainError extends Error {
