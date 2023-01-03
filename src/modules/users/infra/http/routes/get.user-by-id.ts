5c514c985d86c41585ac7fa68dcc3fac80bb6cce
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-01-02 22:49:25 -0300

    feat: add request validation pipe on get user by id route

--- src/modules/users/infra/http/routes/get.user-by-id.ts
+++ src/modules/users/infra/http/routes/get.user-by-id.ts
@@ -1,9 +1,11 @@
 import { Request } from "express";
+import Joi from "joi";
 import EndpointBuilder from "~services/webserver/express/utils/endpoint-builder";
 import DependencyInjection from "~shared/dependency-injection";
 import GetUser from "~modules/users/use-cases/get-user";
 import { Http } from "~services/webserver/types";
 import { UserHttpErrors } from "~modules/users/infra/http/errors";
+import requestValidation from "~services/webserver/express/pipes/request-validation.pipe";
 
 interface GetUserRequest extends Request {
   params: {
@@ -13,7 +15,13 @@ interface GetUserRequest extends Request {
 
 export default EndpointBuilder.new("/api/get-user/:id")
   .setHttpMethod(Http.Methods.GET)
-  // TODO: Add Joi PIPE
+  .addPipe(
+    requestValidation({
+      params: Joi.object({
+        id: Joi.string(),
+      }),
+    })
+  )
   .setHandler(async (req: GetUserRequest, res) => {
     const { id } = req.params;
     const getUser = DependencyInjection.resolve(GetUser);
