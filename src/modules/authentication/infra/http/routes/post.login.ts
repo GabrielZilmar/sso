d8b0ec4b2d56cc9a04735c3de631944a4c835985
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-01-13 22:27:59 -0300

    feat: add authentication login route

---
+++ src/modules/authentication/infra/http/routes/post.login.ts
@@ -0,0 +1,41 @@
+import { Request } from "express";
+import Joi from "joi";
+import LoginUseCase from "~modules/authentication/use-case/login";
+import requestValidation from "~services/webserver/express/pipes/request-validation.pipe";
+import EndpointBuilder from "~services/webserver/express/utils/endpoint-builder";
+import { Http } from "~services/webserver/types";
+import DependencyInjection from "~shared/dependency-injection";
+
+interface LoginRequest extends Request {
+  body: {
+    email: string;
+    password: string;
+  };
+}
+
+export default EndpointBuilder.new("/api/login")
+  .setHttpMethod(Http.Methods.POST)
+  .addPipe(
+    requestValidation({
+      body: Joi.object({
+        email: Joi.string().email().required(),
+        password: Joi.string().required(),
+      }),
+    })
+  )
+  .setHandler(async (req: LoginRequest, res) => {
+    const { body: loginData } = req;
+
+    const loginUseCase = DependencyInjection.resolve(LoginUseCase);
+    const userAuthenticatedOrError = await loginUseCase.execute(loginData);
+
+    if (userAuthenticatedOrError.isLeft()) {
+      const statusError = userAuthenticatedOrError.value.status;
+      const messageError = userAuthenticatedOrError.value.message;
+
+      res.status(statusError).send({ error: messageError });
+      return;
+    }
+
+    res.status(Http.Status.OK).send(userAuthenticatedOrError.value);
+  });
