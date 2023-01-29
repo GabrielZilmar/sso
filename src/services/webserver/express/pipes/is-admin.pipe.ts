44829e4d579e840130e4955d1f901ba60b3a6a1d
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-01-29 12:02:18 -0300

    feat: add is-admin pipe

---
+++ src/services/webserver/express/pipes/is-admin.pipe.ts
@@ -0,0 +1,22 @@
+import { UserDTO } from "~modules/users/dto/user-dto";
+import { IPipe } from "~services/webserver/express/utils/endpoint-builder";
+import { Http } from "~services/webserver/types";
+
+const isAdminPipe: IPipe = (req, res, next): void => {
+  const { token } = req.state as {
+    token: UserDTO;
+  };
+  const isAdmin = { token };
+
+  if (!isAdmin) {
+    res.status(Http.Status.FORBIDDEN).send({
+      message:
+        "You don't have permission to access this feature, it's exclusive for admins.",
+    });
+    return;
+  }
+
+  next();
+};
+
+export default isAdminPipe;
