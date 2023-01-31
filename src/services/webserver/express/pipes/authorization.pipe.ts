730f5f7c88c26b182fcc3dea92f3e47da046877c
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-01-30 22:50:17 -0300

    feat: improve authorization pipe to check req body and params

--- src/services/webserver/express/pipes/authorization.pipe.ts
+++ src/services/webserver/express/pipes/authorization.pipe.ts
@@ -2,8 +2,20 @@ import { UserDTO } from "~modules/users/dto/user-dto";
 import { IPipe } from "~services/webserver/express/utils/endpoint-builder";
 import { Http } from "~services/webserver/types";
 
+const userCanAccess = (
+  params: Record<string, unknown>,
+  token: Partial<UserDTO>
+): boolean => {
+  return Object.keys(params).every((item) => {
+    if (token[item]) {
+      return token[item] === params[item];
+    }
+    return true;
+  });
+};
+
 const authorizationPipe: IPipe = (req, res, next): void => {
-  const { body } = req;
+  const { body, params } = req;
   const { token } = req.state as {
     token: UserDTO;
   };
@@ -14,13 +26,7 @@ const authorizationPipe: IPipe = (req, res, next): void => {
     return;
   }
 
-  const canAccess = Object.keys(body).every((item) => {
-    if (token[item]) {
-      return token[item] === body[item];
-    }
-    return true;
-  });
-
+  const canAccess = userCanAccess(params, token) && userCanAccess(body, token);
   if (!canAccess) {
     res.status(Http.Status.FORBIDDEN).send({
       message: "Forbidden. You don't have permission to access this feature.",
