5106ba22c73929bdebfd1d9f4059ff2a04f6e298
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2023-09-12 15:07:36 -0300

    fix: database-config paths

--- src/config/database-config.ts
+++ src/config/database-config.ts
@@ -1,10 +1,9 @@
 import { config } from "dotenv";
+import { join } from "path";
 
 config();
 
 const getDatabasePort = () => Number((process.env.DB_PORT as string) || 0);
-const environment = process.env.NODE_ENV as string;
-const isProd = environment === "prod";
 
 const databaseConfig = {
   databaseUrl: process.env.DATABASE_URL as string,
@@ -14,15 +13,15 @@ const databaseConfig = {
   username: process.env.USERNAME as string,
   password: process.env.PASSWORD as string,
   database: process.env.DATABASE as string,
-  entitiesPath: isProd
-    ? "modules/**/entity/*{.js}"
-    : "src/modules/**/entity/*{.ts,.js}",
-  migrationsPath: isProd
-    ? "services/database/typeorm/migrations/*{.js}"
-    : "src/services/database/typeorm/migrations/*{.ts,.js}",
-  subscribersPath: isProd
-    ? "services/database/typeorm/subscribers/*{.js}"
-    : "src/services/database/typeorm/subscribers/*{.ts,.js}",
+  entitiesPath: join(__dirname, "../modules/**/entity/*.{ts,js}"),
+  migrationsPath: join(
+    __dirname,
+    "../services/database/typeorm/migrations/*{.ts,.js}"
+  ),
+  subscribersPath: join(
+    __dirname,
+    "../services/database/typeorm/subscribers/*{.ts,.js}"
+  ),
 };
 
 export default databaseConfig;
